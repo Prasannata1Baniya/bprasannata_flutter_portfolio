45953ea1c3d77bd9af748943f3abc6eca5c3efdd
Prasannata Baniya <prasannata250baniya@gmail.com>
2026-06-30 21:38:35 +0545

    Updated

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "714849869" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1429722474" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
